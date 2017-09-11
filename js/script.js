ae23a0a66dcadf25156cd73172e68711daf8185a
LukeWlodarczyk <wlod_a_rczyk@interia.pl>
2017-09-11 06:20:10 +0200

    Section 4 showSelctMenu js

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function(){
 
 
   //IMAGES SLIDER
-  
+
   const prev = document.querySelector(".leftArrow");
   const next = document.querySelector(".rightArrow");
   const images = document.querySelectorAll(".chair");
@@ -32,6 +32,22 @@ document.addEventListener("DOMContentLoaded", function(){
     images[index].classList.add("visible");
 
   };
+
+
+
+  // ORDER CHAIR SECTION
+
+  // SHOW SELECT MENU
+  const listLabels = document.querySelectorAll(".list_label");
+
+  listLabels.forEach( function(listLabel){
+    listLabel.addEventListener('click', showSelectMenu)
+  });
+
+  function showSelectMenu(e) {
+     this.nextElementSibling.classList.toggle('visible')
+  }
+
 
 
 

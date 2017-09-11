84ba1a3afab90ea6581fa5aede3bffaf286a825f
LukeWlodarczyk <wlod_a_rczyk@interia.pl>
2017-09-11 19:21:54 +0200

    Section 4 js

--- js/script.js
+++ js/script.js
@@ -5,31 +5,31 @@ document.addEventListener("DOMContentLoaded", function(){
 
   const prev = document.querySelector(".leftArrow");
   const next = document.querySelector(".rightArrow");
-  const images = document.querySelectorAll(".chair");
+  const imagesSlider = document.querySelectorAll(".chairSlider");
 
   prev.addEventListener('click', prevSlide)
   next.addEventListener('click', nextSlide)
 
   let index = 0;
-  images[index].classList.add("visible");
+  imagesSlider[index].classList.add("visible");
 
   function prevSlide(e){
-    images[index].classList.remove("visible");
+    imagesSlider[index].classList.remove("visible");
     index--;
     if(index < 0){
       index = 2;
     };
-    images[index].classList.add("visible");
+    imagesSlider[index].classList.add("visible");
 
   };
 
   function nextSlide(e){
-    images[index].classList.remove("visible");
+    imagesSlider[index].classList.remove("visible");
     index++;
     if(index > 2 ){
       index = 0;
     };
-    images[index].classList.add("visible");
+    imagesSlider[index].classList.add("visible");
 
   };
 
@@ -38,19 +38,81 @@ document.addEventListener("DOMContentLoaded", function(){
   // ORDER CHAIR SECTION
 
   // SHOW SELECT MENU
-  const listLabels = document.querySelectorAll(".list_label");
+  const listArrows = document.querySelectorAll(".list_arrow");
 
-  listLabels.forEach( function(listLabel){
-    listLabel.addEventListener('click', showSelectMenu)
+  listArrows.forEach( function(listArrow){
+    listArrow.addEventListener('click', showSelectMenu)
   });
 
   function showSelectMenu(e) {
      this.nextElementSibling.classList.toggle('visible')
   }
 
+  //CHECK EVENT
+
+  const lis = document.querySelectorAll(".list_panel li");
+  const check = document.querySelector("#transport");
+  const checkVal = document.querySelector("#transport").dataset.transportPrice;
+  const sum = document.querySelector(".sum");
+  let transportPrc = 0;
+
+  check.addEventListener("click", transport);
+
+  function transport(e){
+    if(check.checked === true){
+      document.querySelector(".panel_left .transport").innerHTML = "Transport";
+      transportPrc = document.querySelector(".panel_right .transport").innerHTML = checkVal;
+    } else{
+      document.querySelector(".panel_left .transport").innerHTML = "Transport";
+      transportPrc = document.querySelector(".panel_right .transport").innerHTML = 0;
+    };
+
+    totalPrice();
+  };
 
 
+  //SELECT EVENTS
 
+  let colorPrc = 0;
+  let typePrc = 0;
+  let patternPrc = 0;
+
+  lis.forEach(function(elem){
+		elem.addEventListener("click", select);
+	});
+
+
+	function select(e){
+    const imageOrder = document.querySelector('.chairOrder');
+		const typePrice = this.dataset.typePrice;
+		const materialPrice = this.dataset.materialPrice;
+    const colorPrice = this.dataset.colorPrice
+		const text = this.innerHTML;
+		const listLabel = this.parentElement.parentElement.firstElementChild;
+
+		listLabel.textContent = text;
+		listLabel.style.color = "#00c4aa";
+		this.parentElement.classList.toggle("visible");
+
+		if(typePrice != undefined){
+			document.querySelector(".panel_left .title").innerHTML = text;
+			typePrc = document.querySelector(".panel_right .title").innerHTML = typePrice;
+      imageOrder.src = `image/${text}.png`
+		}else if(colorPrice != undefined){
+			document.querySelector(".panel_left .color").innerHTML = text;
+			colorPrc = document.querySelector(".panel_right .color").innerHTML = colorPrice;
+		}else if(materialPrice != undefined){
+			document.querySelector(".panel_left .pattern").innerHTML = text;
+			patternPrc = document.querySelector(".panel_right .pattern").innerHTML = materialPrice;
+		};
+
+		totalPrice();
+
+	};
+
+  function totalPrice() {
+    sum.innerHTML = (parseInt(typePrc) + parseInt(transportPrc) + parseInt(colorPrc) + parseInt(patternPrc))+ " zł";
+  }
 
 
 });
